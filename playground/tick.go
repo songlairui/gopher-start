5b79971a608f450976809f3bf343a360e63114b8
songlr <songlr@mingyuanyun.com>
2018-08-17 11:44:56 +0800

    play chan

---
+++ playground/tick.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick.", time.Now())
+		case <-boom:
+			fmt.Println("BOOM!", time.Now())
+			return
+		default:
+			fmt.Println("    .", time.Now())
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+}
